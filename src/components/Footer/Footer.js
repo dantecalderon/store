26778e63911cc1af6e76cf878b4c2925d528dc7a
dantehemerson <dantehemerson@gmail.com>
2018-09-29 16:02:44 -0500

    Test styled-components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+	background: skyblue;
+`
 
 export default props => (
-	<div>
+	<Container>
 		<p>(c) Some organization 2018 - All rights reserved</p>
-	</div>
+	</Container>
 )
