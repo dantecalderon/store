d095f1bf99b7a517d7c7c10f4cc5d14856f7c049
dantehemerson <dantehemerson@gmail.com>
2018-10-04 22:34:00 -0500

    :nail_care: Add purecss framework

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 import Footer from '../components/Footer'
 
+function holaMundo (){
+	console.log('Hola Mundo')
+	if(this == 8) {
+		return 0
+	}
+}
 export default () => <div>
 	Hello world!
 	<Footer />
