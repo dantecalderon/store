13b32dad667729b7ba8ab94ad682f273d23e8f06
dantehemerson <dantehemerson@gmail.com>
2018-10-09 07:46:24 -0500

    Install carousel styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import Footer from '../components/Footer'
 import Layout from '../components/Layouts/Default'
+import Carousel from '../components/HomeCarousel'
 
 export default () => (
 	<Layout>
+		<Carousel/>
 		<h1>Gatsby Store</h1>
 		<Footer/>
 	</Layout>
