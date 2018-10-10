74d74665c984e49c13a2b9893c9f3acb956989d5
dantehemerson <dantehemerson@gmail.com>
2018-10-10 04:24:17 -0500

    Add netlify-cms
    No se está renderizando correctamente la pagina, al actualizar la página
    no se cargan algunos estilos pero al cambiar de paginas sin recargar se
    aplican los estilos, no se porque esté sucediendo esto. Estoy intentando
    actualizar las dependencias y agregar este plugin(netlify cms), si no
    funciona supongo que lo resolvería el plugin offline de gatsby pero no
    estoy seguro. Es raro que esto pase no he visto ningún sitio que haga
    eso aúnque con la actualización a la v2 de gatsby de mi pagína web tuve
    ese problema pero sólo en el entorno de desarrollo. En fín intentaré
    resolver el problema marcaré esto como un issue para que quede
    documentado. :smile:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
 	plugins: [
 		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-netlify-cms'
+		'gatsby-plugin-netlify-cms',
+		{
+		  resolve: 'gatsby-plugin-netlify',
+		  options: {		    		  
+		    mergeSecurityHeaders: true,
+		    mergeLinkHeaders: true,
+		    mergeCachingHeaders: true,
+		    generateMatchPathRewrites: true
+		  }
+		}
 	]
 }
