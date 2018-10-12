be813d0a9ac8107e61ca34836482a77084185ad4
dantehemerson <dantehemerson@gmail.com>
2018-10-11 22:00:35 -0500

    Show product data

--- src/templates/ProductTemplate.js
+++ src/templates/ProductTemplate.js
@@ -4,8 +4,35 @@ import styled from 'styled-components'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layouts/Default'
 
-export default props => (
-	<Layout>
-		Esto es un producto
-	</Layout>
-)
+export default props => {
+	const product = props.data.markdownRemark
+	console.log(props.data)
+	return (
+		<Layout>
+			<h1>{ product.frontmatter.title }</h1>
+			<p>{ product.frontmatter.price }</p>
+			<p>{ product.frontmatter.color }</p>
+			<p>{ product.frontmatter.categories }</p>
+			<img src={ product.frontmatter.thumbnail }/>			
+		</Layout>
+	)	
+}
+
+export const pageQuery = graphql`
+	query($slug: String!) {
+		markdownRemark(fields: { slug: { eq: $slug } }) {
+			htmlAst
+			fields {
+				slug
+			}
+			frontmatter {
+				title
+				price
+				color
+				categories
+				color
+				thumbnail
+			}
+		}
+	}
+`
